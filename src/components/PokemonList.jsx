3d26024521653eb3e7f5bafe0c2bfcfa44398597
JDargon <argon181120@gmail.com>
2026-03-09 21:21:42 +0000

    error de sincronía

---
+++ src/components/PokemonList.jsx
@@ -0,0 +1,13 @@
+import PokemonCard from "./PokemonCard"
+
+function PokemonList() {
+
+  return (
+    <ul className="Pokemon-list">
+        <PokemonCard/>
+    </ul>
+  )
+}
+
+export default PokemonList
+
