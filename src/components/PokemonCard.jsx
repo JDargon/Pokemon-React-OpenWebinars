275843400434baad38a85af5875151147e1e4d66
Dargon <argon181120@gmail.com>
2026-03-10 14:29:58 +0000

    Explicando sincronía

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -4,22 +4,36 @@ function PokemonCard() {
 
   const [pokemon, setPokemon] = useState({});
 
+  // useEffect(() => {
+  //   fetch("https://pokeapi.co/api/v2/pokemon/1")
+  //     .then((response) => response.json())
+  //     .then((data) => setPokemon(data))
+  // }, [])
+
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon/1")
       .then((response) => response.json())
-      .then((data) => setPokemon(data))
+      .then((data) => {
+        setTimeout(() => {
+          setPokemon(data)
+        }, 2000);
+      })
   }, [])
 
   return (
-    <li className="pokemon-card">
-      <h2 className="pokemon-name">{pokemon.name}</h2>
-      <img
-        src={pokemon.sprites.front_default}
-        alt="pokemon img"
-        className="pokemon-img" />
-      <h3 className="text">HP: {pokemon.stats[0].base_stat}</h3>
-    </li>
-  )
+    pokemon.id ? (
+      <li className="pokemon-card">
+        <h2 className="pokemon-name">{pokemon.name}</h2>
+        <img
+          src={pokemon.sprites.front_default}
+          alt="pokemon img"
+          className="pokemon-img" />
+        <h3 className="text">HP: {pokemon.stats[0].base_stat}</h3>
+      </li>
+    ) : (
+      <p className="loading">Loading...</p>
+    )
+  );
 }
 
-export default PokemonCards
+export default PokemonCard
